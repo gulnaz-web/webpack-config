e2ccebf2ebb645da12943a3076d9ef217beaeff4
yetimol <yetimol@MacBook-Pro-yetimol.local>
2025-10-31 21:12:04 +0300

    added typescript config

---
+++ webpack.config.ts
@@ -0,0 +1,33 @@
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import webpack from "webpack";
+import path from "path";
+
+const config: webpack.Configuration = {
+  entry: path.resolve(__dirname, "src", "index.ts"),
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "[contenthash].[name].js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public", "index.html"),
+    }),
+    new webpack.ProgressPlugin(),
+  ],
+};
+
+export default config;
